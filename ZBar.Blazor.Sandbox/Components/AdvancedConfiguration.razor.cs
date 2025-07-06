f442d21356e93fd1f47b13f8893e045f1a639dfc
Drew Miller <drew.miller35@yahoo.com>
2025-07-05 21:48:38 -0500

    Added ability to configure barcode types to scan for in the camera demo sandbox

---
+++ ZBar.Blazor.Sandbox/Components/AdvancedConfiguration.razor.cs
@@ -0,0 +1,133 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using ZBar.Blazor.Config;
+
+namespace ZBar.Blazor.Sandbox.Components
+{
+    public partial class AdvancedConfiguration
+    {
+        [CascadingParameter] BlazoredModalInstance ModalInstance { get; set; }
+
+        [Parameter] public BarcodeType ScanFor { get; set; }
+
+        private bool AllBarcodeTypes { get; set; }
+        private bool CustomUPCA { get; set; }
+        private bool CustomUPCE { get; set; }
+        private bool CustomISBN13 { get; set; }
+        private bool CustomISBN10 { get; set; }
+        private bool CustomEAN13 { get; set; }
+        private bool CustomEAN8 { get; set; }
+        private bool CustomEAN5 { get; set; }
+        private bool CustomEAN2 { get; set; }
+        private bool CustomI25 { get; set; }
+        private bool CustomDatabar { get; set; }
+        private bool CustomDatabarExpanded { get; set; }
+        private bool CustomComposite { get; set; }
+        private bool CustomCodabar { get; set; }
+        private bool CustomCode39 { get; set; }
+        private bool CustomCode93 { get; set; }
+        private bool CustomCode128 { get; set; }
+
+        private bool CustomQR { get; set; }
+        private bool CustomQRS { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            AllBarcodeTypes = ScanFor == BarcodeType.ALL;
+            if (!AllBarcodeTypes)
+            {
+                CustomUPCA = ScanFor.HasFlag(BarcodeType.UPC_A);
+                CustomUPCE = ScanFor.HasFlag(BarcodeType.UPC_E);
+                CustomISBN13 = ScanFor.HasFlag(BarcodeType.ISBN_13);
+                CustomISBN10 = ScanFor.HasFlag(BarcodeType.ISBN_10);
+                CustomEAN13 = ScanFor.HasFlag(BarcodeType.EAN_13);
+                CustomEAN8 = ScanFor.HasFlag(BarcodeType.EAN_8);
+                CustomEAN5 = ScanFor.HasFlag(BarcodeType.EAN_5);
+                CustomEAN2 = ScanFor.HasFlag(BarcodeType.EAN_2);
+                CustomI25 = ScanFor.HasFlag(BarcodeType.I25);
+                CustomDatabar = ScanFor.HasFlag(BarcodeType.DATABAR);
+                CustomDatabarExpanded = ScanFor.HasFlag(BarcodeType.DATABAR_EXPANDED);
+                CustomComposite = ScanFor.HasFlag(BarcodeType.COMPOSITE);
+                CustomCodabar = ScanFor.HasFlag(BarcodeType.CODABAR);
+                CustomCode39 = ScanFor.HasFlag(BarcodeType.CODE_39);
+                CustomCode93 = ScanFor.HasFlag(BarcodeType.CODE_93);
+                CustomCode128 = ScanFor.HasFlag(BarcodeType.CODE_128);
+                CustomQR = ScanFor.HasFlag(BarcodeType.QR_CODE);
+                CustomQRS = ScanFor.HasFlag(BarcodeType.QR_CODE_SECURE);
+            }
+        }
+
+        private void EnableAllBarcodes()
+        {
+            CustomUPCA = false;
+            CustomUPCE = false;
+            CustomISBN13 = false;
+            CustomISBN10 = false;
+            CustomEAN13 = false;
+            CustomEAN8 = false;
+            CustomEAN5 = false;
+            CustomEAN2 = false;
+            CustomI25 = false;
+            CustomDatabar = false;
+            CustomDatabarExpanded = false;
+            CustomComposite = false;
+            CustomCodabar = false;
+            CustomCode39 = false;
+            CustomCode93 = false;
+            CustomCode128 = false;
+            CustomQR = false;
+            CustomQRS = false;
+
+            AllBarcodeTypes = true;
+        }
+
+        private async Task CloseOk()
+        {
+            var scanFor = BarcodeType.ALL;
+            if (!AllBarcodeTypes) {
+                scanFor = 0;
+                if (CustomUPCA) scanFor = scanFor | BarcodeType.UPC_A;
+                if (CustomUPCE) scanFor = scanFor | BarcodeType.UPC_E;
+                if (CustomISBN13) scanFor = scanFor | BarcodeType.ISBN_13;
+                if (CustomISBN10) scanFor = scanFor | BarcodeType.ISBN_10;
+                if (CustomEAN13) scanFor = scanFor | BarcodeType.EAN_13;
+                if (CustomEAN8) scanFor = scanFor | BarcodeType.EAN_8;
+                if (CustomEAN5) scanFor = scanFor | BarcodeType.EAN_5;
+                if (CustomEAN2) scanFor = scanFor | BarcodeType.EAN_2;
+                if (CustomI25) scanFor = scanFor | BarcodeType.I25;
+                if (CustomDatabar) scanFor = scanFor | BarcodeType.DATABAR;
+                if (CustomDatabarExpanded) scanFor = scanFor | BarcodeType.DATABAR_EXPANDED;
+                if (CustomComposite) scanFor = scanFor | BarcodeType.COMPOSITE;
+                if (CustomCodabar) scanFor = scanFor | BarcodeType.CODABAR;
+                if (CustomCode39) scanFor = scanFor | BarcodeType.CODE_39;
+                if (CustomCode93) scanFor = scanFor | BarcodeType.CODE_93;
+                if (CustomCode128) scanFor = scanFor | BarcodeType.CODE_128;
+                if (CustomQR) scanFor = scanFor | BarcodeType.QR_CODE;
+                if (CustomQRS) scanFor = scanFor | BarcodeType.QR_CODE_SECURE;
+
+                if (scanFor == 0) scanFor = BarcodeType.ALL;
+            }
+
+            var result = new Result(
+                scanFor != ScanFor ? scanFor : null
+            );
+
+            await ModalInstance.CloseAsync(ModalResult.Ok(result));
+        }
+
+        private async Task Cancel() => await ModalInstance.CancelAsync();
+
+        public record Result
+        {
+            public BarcodeType? ScanFor { get; set; }
+
+            public Result(BarcodeType? scanFor)
+            {
+                ScanFor = scanFor;
+            }
+        }
+    }
+}
---
+++ ZBar.Blazor.Sandbox/Pages/CameraDemo.razor.cs
@@ -0,0 +1,107 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using ZBar.Blazor.Components;
+using ZBar.Blazor.Config;
+using ZBar.Blazor.Dtos;
+using ZBar.Blazor.Sandbox.Components;
+
+namespace ZBar.Blazor.Sandbox.Pages
+{
+    public partial class CameraDemo
+    {
+        [CascadingParameter] public IModalService Modal { get; set; }
+
+        private ZBarCamera Camera;
+
+        private bool AutoScan { get; set; } = true;
+        private int AutoScanInterval { get; set; } = 1000;
+        private BarcodeType ScanFor { get; set; } = BarcodeType.UPC_A;
+        private bool Verbose { get; set; } = true;
+        private CameraViewType CameraView { get; set; } = CameraViewType.VideoFeed;
+
+        private HardwareDevice[] AvailableHardwareDevices;
+        private string SelectedHardwareDeviceId;
+
+        private Barcode[] FoundBarcodes;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                AvailableHardwareDevices = await Camera.GetAvailableDevices();
+                StateHasChanged();
+            }
+        }
+
+        private async Task StartVideoFeed()
+        {
+            if (SelectedHardwareDeviceId != null)
+            {
+                await Camera.StartVideoFeed(SelectedHardwareDeviceId);
+            }
+        }
+
+        private async Task EndVideoFeed()
+        {
+            await Camera.EndVideoFeed();
+        }
+
+        private async Task Scan()
+        {
+            await Camera.Scan();
+        }
+
+        private async Task OpenAdvancedConfig()
+        {
+            var parameters = new ModalParameters()
+                .Add(nameof(AdvancedConfiguration.ScanFor), ScanFor);
+
+            var modal = Modal.Show<AdvancedConfiguration>("Advanced Configuration", parameters);
+            var result = await modal.Result;
+
+            if(!result.Cancelled) {
+                var config = result.Data as AdvancedConfiguration.Result;
+                if (config.ScanFor.HasValue) ScanFor = config.ScanFor.Value;
+            }
+        }
+
+        private string GetScanForDisplay()
+        {
+            if (ScanFor == BarcodeType.ALL) return "All Types";
+
+            var flagsCount = 0;
+            foreach (var type in Enum.GetValues<BarcodeType>())
+                if (ScanFor.HasFlag(type)) flagsCount++;
+
+            if (flagsCount > 1) return "Multiple Types";
+            if (ScanFor.HasFlag(BarcodeType.UPC_A)) return "UPC-A";
+            if (ScanFor.HasFlag(BarcodeType.UPC_E)) return "UPC-E";
+            if (ScanFor.HasFlag(BarcodeType.ISBN_13)) return "ISBN-13";
+            if (ScanFor.HasFlag(BarcodeType.ISBN_10)) return "ISBN-10";
+            if (ScanFor.HasFlag(BarcodeType.EAN_13)) return "EAN-13";
+            if (ScanFor.HasFlag(BarcodeType.EAN_8)) return "EAN-8";
+            if (ScanFor.HasFlag(BarcodeType.EAN_5)) return "EAN-5";
+            if (ScanFor.HasFlag(BarcodeType.EAN_2)) return "EAN-2";
+            if (ScanFor.HasFlag(BarcodeType.I25)) return "I25";
+            if (ScanFor.HasFlag(BarcodeType.DATABAR)) return "GS1 DataBar";
+            if (ScanFor.HasFlag(BarcodeType.DATABAR_EXPANDED)) return "GS1 DataBar (Expanded)";
+            if (ScanFor.HasFlag(BarcodeType.COMPOSITE)) return "GS1 Composite";
+            if (ScanFor.HasFlag(BarcodeType.CODABAR)) return "GS1 Codabar";
+            if (ScanFor.HasFlag(BarcodeType.CODE_39)) return "Code-39";
+            if (ScanFor.HasFlag(BarcodeType.CODE_93)) return "Code-93";
+            if (ScanFor.HasFlag(BarcodeType.CODE_128)) return "Code-128";
+            if (ScanFor.HasFlag(BarcodeType.QR_CODE)) return "QR Code";
+            if (ScanFor.HasFlag(BarcodeType.QR_CODE_SECURE)) return "QR Code (Secure)";
+
+            return "N/A";
+        }
+
+        private void OnBarcodesFound(ScanResult scanResult)
+        {
+            FoundBarcodes = scanResult.Barcodes;
+            StateHasChanged();
+        }
+    }
+}
